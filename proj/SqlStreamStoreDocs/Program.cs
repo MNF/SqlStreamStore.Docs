311fb5c25faf74dff378ec698bd4482bb80f0496
Damian Hickey <dhickey@gmail.com>
2017-01-26 00:12:59 +0100

    Quickstart

---
+++ proj/SqlStreamStoreDocs/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace SqlStreamStoreDocs
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using SqlStreamStore;
+    using SqlStreamStore.Streams;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task.Run(QuickStart).GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        public static async Task QuickStart()
+        {
+            var ct = CancellationToken.None;
+
+            // Create an in-memory store
+            var store = new InMemoryStreamStore();
+
+            // Create a new message
+            var message = JsonConvert.SerializeObject(new { Name = "foo" });
+            var newStreamMessage = new NewStreamMessage(Guid.NewGuid(), "my-message-type", message);
+
+            // Append to a stream
+            await store.AppendToStream("stream-1", ExpectedVersion.NoStream, newStreamMessage, ct);
+
+            // Read the stream
+            var page = await store.ReadStreamForwards("stream-1", StreamVersion.Start, 10, ct);
+
+            Console.WriteLine(page.StreamId);           // stream-1
+            Console.WriteLine(page.Status);             // Success
+            Console.WriteLine(page.IsEnd);              // True
+            Console.WriteLine(page.FromStreamVersion);  // 0
+            Console.WriteLine(page.LastStreamVersion);  // 0
+            Console.WriteLine(page.NextStreamVersion);  // 1
+            Console.WriteLine(page.ReadDirection);      // Forward
+            Console.WriteLine(page.Messages.Length);    // 1
+
+            Console.WriteLine(page.Messages[0].StreamId);       // stream-1
+            Console.WriteLine(page.Messages[0].CreatedUtc);     // The date time
+            Console.WriteLine(page.Messages[0].MessageId);      // A guid
+            Console.WriteLine(page.Messages[0].Position);       // 0
+            Console.WriteLine(page.Messages[0].StreamVersion);  // 0
+            Console.WriteLine(page.Messages[0].Type);           // my-message-type
+
+            // Loading the message content is async
+            dynamic messageRead = await page.Messages[0].GetJsonDataAs<dynamic>(ct);
+
+            Console.WriteLine(messageRead.Name); // "foo"
+
+        }
+    }
+}
--- source/quickstart.rst
+++ source/quickstart.rst
@@ -7,13 +7,18 @@ The following platforms are supported:
 * .NET 4.6 and up.
 * .NET Core / NetStandard in due course.
 
+This example show the simplest way to append a message to a stream and read it
+back again.
+
 .. code-block:: c#
 
     // Create an in-memory store
     var store = new InMemoryStreamStore();
 
     // Create a new message
-    var newStreamMessage = new NewStreamMessage(Guid.NewGuid(), "my-message-type", "{ \"name\": \"foo\" }");
+    var message = JsonConvert.SerializeObject(new { Name = "foo" });
+    var messageId = Guid.NewGuid();
+    var newStreamMessage = new NewStreamMessage(messageId, "my-message-type", message);
 
     // Append to a stream
     await store.AppendToStream("stream-1", ExpectedVersion.NoStream, newStreamMessage, ct);
@@ -21,5 +26,7 @@ The following platforms are supported:
     // Read the stream
     var page = await store.ReadStreamForwards("stream-1", StreamVersion.Start, 10, ct);
 
-    Console.WriteLine(page.Status);
-    Console.WriteLine(page.Messages[0].Type);
+    // Loading the message content is async (but prefetched by default)
+    string messageJson = await page.Messages[0].GetJsonData(ct);
+
+    Console.WriteLine(messageJson); // "foo"
